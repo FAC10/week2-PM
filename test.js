2f2ac01e025ec3c4b4d00e287572bf72ea1e9c6e
pbywater <philippabywater@gmail.comphilippabywater$ git config --global user.email philippabywater@gmail.comgit config --global user.email philippabywater@gmail.comgit config --global user.name pbywatergit config --global user.email test@youremail.com git config --global user.name Bhumi>
2017-03-01 11:48:01 +0000

    adding test to check stopwatch starts at 0
    relates to #1

---
+++ test.js
@@ -0,0 +1,17 @@
+test('stopwatch should be showing zeros when the page is loaded', function(assert) {
+  var result = document.getElementById("stopwatch__hours").innerHTML;
+  var expected = "00";
+  assert.equal(result, expected);
+  });
+
+test('stopwatch should be showing zeros when the page is loaded', function(assert) {
+  var result = document.getElementById("stopwatch__minutes").innerHTML;
+    var expected = "00";
+    assert.equal(result, expected);
+    });
+
+  test('stopwatch should be showing zeros when the page is loaded', function(assert) {
+      var result = document.getElementById("stopwatch__seconds").innerHTML;
+      var expected = "00";
+      assert.equal(result, expected);
+      });
