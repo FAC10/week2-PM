91d59f48c5c2de1d6a0574accb6cac348b118cf2
pbywater <philippabywater@gmail.comphilippabywater$ git config --global user.email philippabywater@gmail.comgit config --global user.email philippabywater@gmail.comgit config --global user.name pbywatergit config --global user.email test@youremail.com git config --global user.name Bhumi>
2017-03-01 12:27:22 +0000

    stopwatch now starts counting on start button click
    relates to #2

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 
 
+// Checks reset when 'Reset' button is clicked
+
 document.getElementById('stopwatch__buttons__reset').addEventListener("click", function(){
 test('stopwatch should be showing zeros when the page is loaded', function(assert) {
 
@@ -22,3 +24,18 @@ test('stopwatch should be showing zeros when the page is loaded', function(asser
       });
 
       });
+
+// Checks behaviour of start button
+document.getElementById('stopwatch__buttons__start').addEventListener("click", function(){
+  window.setTimeout( function (){
+
+
+test('stopwatch second start should change once we initiate start button and >1s has passed', function(assert) {
+
+
+  var result = document.getElementById("stopwatch__seconds").innerHTML;
+  var expected = "00";
+  assert.notEqual(result, expected);
+  });
+  }, 1000);
+  });
--- timer.js
+++ timer.js
@@ -1,8 +1,14 @@
-// var myVar = setInterval(myTimer, 1000);
-//
-// function myTimer() {
-//     var d = new Date();
-//     document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-//     console.log(myTimer);
-//     console.log(d);
-// }
+
+document.getElementById('stopwatch__buttons__start').addEventListener("click", function(){
+
+setInterval(stopwatch__start, 1000);
+
+function stopwatch__start() {
+  var seconds = document.getElementById('stopwatch__seconds').innerHTML;
+  seconds = parseInt(seconds);
+  seconds ++;
+  document.getElementById('stopwatch__seconds').innerHTML = seconds;
+  console.log(seconds);
+}
+
+});
